3c80c0e04a22281e487d23d377d46414ad149538
Jordan Wade <jjwade31@gmail.com>
2014-12-10 14:48:36 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,6 +18,13 @@ then
   ln -s ~/.dotfiles/bash_profile ~/.bash_profile
   echo "--> Symlinked ~/.dotfiles/bash_profile to ~/.bash_profile"
 
+  echo
+  echo "--------------------------------------------------------------"
+  echo "--> Setting up Git..."
+  echo "--------------------------------------------------------------"
+  ln -s ~/.dotfiles/git/gitconfig.local ~/.gitconfig.local
+  echo "--> Symlinked ~/.dotfiles/git/gitconfig.local to ~/.gitconfig.local"
+
   source ~/.bash_profile
   echo "--> Sourced ~/.bash_profile"
 
